bb5977ecff40faae13d70005ba8cdd614afdfb21
savaseso <rigbence@gmail.com>
2019-03-16 04:13:18 +0100

    anchestor element changed to a button one

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -11,7 +11,7 @@ const Pagination = ({itemsCount, pageSize, currentPage, onPageChange}) => {
             <ul className="pagination">
                 {pages.map(page =>
                 <li key = {page} className={page===currentPage ? 'page-item active' : 'page-item'}>
-                    <a className="page-link" onClick={()=>onPageChange(page)}>{page}</a>
+                    <button className="page-link" onClick={()=>onPageChange(page)}>{page}</button>
                 </li>)}
                
             </ul>
