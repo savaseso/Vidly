0d91fd3a2aa29f2c13c752c4c2b4c5532ddf8654
savaseso <rigbence@gmail.com>
2019-03-22 02:02:57 +0100

    validating input field

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -3,24 +3,39 @@ import Input from './input';
 
 class LoginForm extends Component {
     state = {
-        account: {username: '', password: ''}
+        account: {username: '', password: ''},
+        errors: {}
     }
+    validate = () => {
+        const errors = {};
+
+        const {account} = this.state;
+        if(account.username.trim()=== '')
+            errors.username = "Username is required";
+        if(account.password.trim()=== '')
+            errors.password = "Password is required";
+        return Object.keys(errors) === 0 ? null : errors;
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const errors = this.validate();
+        this.setState({errors: errors || {}});
+        if(errors) return;
+
+        //call the server
+        console.log('Submitted')
+    }
+
     handleChange = e => {
         const account = {...this.state.account};
         account[e.currentTarget.name] = e.currentTarget.value
         this.setState({account});
     }
 
-    handleSubmit = e => {
-
-        e.preventDefault();
-
-        //call the server
-        console.log('Submitted')
-    }
 
     render() {
-        const {account}= this.state;
+        const {account, errors}= this.state;
         return (
             <div>
                 <h1>Login</h1>
@@ -29,13 +44,16 @@ class LoginForm extends Component {
                      onChange={this.handleChange} 
                      name="username" 
                      value={account.username} 
-                     label="Username" />
+                     label="Username"
+                     error={errors.username}
+                      />
                      <Input 
                      onChange={this.handleChange} 
                      name="password" 
                      value={account.password} 
-                     label="Password" />
-                    
+                     label="Password"
+                     error={errors.password}
+                      />
                     <button className="btn btn-primary">Submit</button>
                 </form>
             </div>
