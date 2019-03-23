cd6293aa2ea848f95ccd2dfa4354a504ee3747d4
savaseso <rigbence@gmail.com>
2019-03-23 17:23:32 +0100

    validation on Change

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -27,10 +27,24 @@ class LoginForm extends Component {
         console.log('Submitted')
     }
 
-    handleChange = e => {
+    validateProperty = ({name, value}) => {
+        if (name === 'username'){
+            if (value.trim() === '') return 'Username is required.'
+        }
+        if (name === 'password'){
+            if (value.trim() === '') return 'Password is required.'
+        }
+    }
+
+    handleChange = ({currentTarget: input}) => {
+        const errors = {...this.state.errors}
+        const errorMessage = this.validateProperty(input);
+        if(errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name]
+
         const account = {...this.state.account};
-        account[e.currentTarget.name] = e.currentTarget.value
-        this.setState({account});
+        account[input.name] = input.value
+        this.setState({ account, errors });
     }
 
 
